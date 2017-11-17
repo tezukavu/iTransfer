db301261aef3901dfe5820dccaa1f757439ceac8
Truc Vu <truc.vu@ericsson.com>
2017-11-17 09:28:19 +0100

    Send url request to php layer

--- app/src/main/java/com/ericsson/etrucvu/itransfer/MainActivity.java
+++ app/src/main/java/com/ericsson/etrucvu/itransfer/MainActivity.java
@@ -1,5 +1,8 @@
 package com.ericsson.etrucvu.itransfer;
 
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,12 +12,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    AlertDialog.Builder builder;
+    EditText receiverId;
+    EditText senderId;
+    EditText amount;
+
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -49,12 +64,65 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
+
+        String server_url = "http://192.168.43.55/dbconfig.php";
+
+        receiverId = (EditText) findViewById(R.id.editReceiverId);
+        final String receiverIdStr = receiverId.getText().toString();
+
+        senderId = (EditText) findViewById(R.id.editSenderId);
+        final String senderIdStr = senderId.getText().toString();
+
+        amount = (EditText) findViewById(R.id.editAmount);
+        final String amountStr = senderId.getText().toString();
+
+        builder = new AlertDialog.Builder(MainActivity.this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                builder.setTitle("Server Response");
+                builder.setMessage("Response :"+response);
+                DialogInterface.OnClickListener onClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // reset all the fields
+                        receiverId.setText("");
+                        senderId.setText("");
+                        amount.setText("");
+                    }
+                };
+
+                builder.setPositiveButton("OK", onClickListener);
+            }
+        }
+                , new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this,"some error found..." + error.toString(),Toast.LENGTH_SHORT).show();
+                error.printStackTrace();
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map <String,String> Params = new HashMap<String, String>();
+                Params.put("Sender",senderIdStr);
+                Params.put("Receiver",receiverIdStr);
+                Params.put("Amount",amountStr);
+                return Params;
+            }
+        };
+        Mysingleton.getInstance(MainActivity.this).addTorequestque(stringRequest);
+
+        /*
         Intent intent;
         intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = (EditText) findViewById(R.id.editSenderId);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+        */
     }
 
 
--- app/src/main/java/com/ericsson/etrucvu/itransfer/Mysingleton.java
+++ app/src/main/java/com/ericsson/etrucvu/itransfer/Mysingleton.java
@@ -1,8 +1,45 @@
 package com.ericsson.etrucvu.itransfer;
 
-/**
- * Created by etrucvu on 11/16/2017.
- */
 
-class Mysingleton {
+import android.content.Context;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+
+public class Mysingleton {
+    private static Mysingleton mInstance;
+    private RequestQueue requestQueue;
+    private static Context mCtx;
+
+
+    private Mysingleton (Context Context)
+    {
+        mCtx = Context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static  synchronized Mysingleton getInstance (Context context)
+    {
+        if (mInstance==null)
+        {
+            mInstance =new Mysingleton(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+
+        if (requestQueue==null)
+        {
+            requestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return requestQueue;
+
+    }
+
+    public<T> void addTorequestque(Request<T> request)
+    {
+        requestQueue.add(request);
+    }
 }
